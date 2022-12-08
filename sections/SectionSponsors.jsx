9318a70f30e99d4837c1197c55a02ea44131f3f0
abc <abc@pop-os.localdomain>
2022-12-08 17:58:45 +0200

    starting tracks

--- sections/SectionSponsors.jsx
+++ sections/SectionSponsors.jsx
@@ -3,11 +3,11 @@ import Image from 'next/image'
 
 function SectionVideo() {
   return (
-    <div className='relative h-[600px] pt-10'>
-      <h2 className='text-lg font-bold text-center pb-32'>People are writting about us!</h2>
+    <div className='relative h-[800px] pt-10 lg:mt-20'>
+      <h2 className='text-lg font-bold text-center pb-32 lg:pb-64'>People are writting about us!</h2>
       <Image src='/strip.svg' width={1544} height={56} className="max-w-none absolute right left-[-160px] top-[100px]"/>
       <h1 className='font-bold text-[40px] leading-[48px] pb-6 text-center'>Medium lenght heading goes here</h1>
-      <p className='pb-10 text-center px-5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+      <p className='pb-10 text-center px-5 lg:text-lg lg:w-[700px] lg:mx-auto'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
       <nav className='flex gap-4 justify-center'>
         <button className='my-button'>Button</button>
         <button className='my-button text-black bg-white border border-black'>Button</button>
--- sections/SectionVideo.jsx
+++ sections/SectionVideo.jsx
@@ -4,14 +4,14 @@ function SectionVideo() {
   return (
     <div className='h-full'>
 
-      <div className='md:flex md:flex-row md:pt-12'>
+      <div className='md:flex md:flex-row md:pt-12 lg:gap-20'>
 
-        <h1 className='font-bold text-[40px] leading-[48px] pb-7 px-6 md:px-0 md:pb-0 text-center md:text-left md:w-6/12'>Medium lenght hero headline goes here</h1>
+        <h1 className='font-bold text-[40px] leading-[48px] lg:text-[56px] lg:leading-[67px] pt-4 pb-6 px-6 md:px-0 md:pb-0 text-center md:text-left md:w-6/12 lg:mr-10'>Medium lenght hero headline goes here</h1>
 
         <div className='md:flex md:flex-col md:w-6/12'>
-          <p className='pb-10 text-left px-5 md:px-0'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat
+          <p className='pb-6 text-left px-5 md:px-0 lg:pl-10'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat
           </p>
-          <nav className='px-5 pb-8 md:px-0'>
+          <nav className='px-5 pb-6 md:px-0 lg:pl-10'>
             <button className='my-button'>Button</button>
             <button className='my-button text-black bg-white border border-black'>Button</button>
           </nav>
@@ -19,7 +19,7 @@ function SectionVideo() {
 
       </div>
 
-      <iframe className='w-full h-[250px] sm:h-[450px] p-3 md:pt-12 md:px-0 md:pb-0' src="https://www.youtube.com/embed/HLQ1cK9Edhc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe className='w-full h-[250px] md:h-[450px] lg:h-[700px] lg:mx-auto p-3 md:pt-12 md:px-0 md:pb-0' src="https://www.youtube.com/embed/HLQ1cK9Edhc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
     </div>
   )
